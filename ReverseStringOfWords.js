917bfbc35fb7191ff15106d9822b7a1242306c27
Ibrahim_A <148215043+ibrahim0102-Auto@users.noreply.github.com>
2025-06-29 23:05:29 +0530

    Delete ReverseStringOfWords.js

--- ReverseStringOfWords.js
+++
@@ -1,9 +0,0 @@
-
-let str="Learn String and Function";
-let value=str.split(" ");
-let reverseString="";
-for(let i=value.length-1;i>=0;i--)
-{
-    reverseString=reverseString + " " + value[i]
-}
-console.log(reverseString)
